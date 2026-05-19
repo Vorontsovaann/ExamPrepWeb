b5005cfacff2064b5597aae73da4b1b2bc98e9bb
Дробжева Анна <vitaminkag671@gmail.com>
2026-05-20 02:21:16 +0300

    Merge remote changes

--- ExamPrepWeb.Tests/obj/Debug/net8.0/ExamPrepWeb.Tests.AssemblyInfo.cs
+++ ExamPrepWeb.Tests/obj/Debug/net8.0/ExamPrepWeb.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExamPrepWeb.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d042fa8f6c8cb677e8622e28999b90c5ee966c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExamPrepWeb.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExamPrepWeb.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
